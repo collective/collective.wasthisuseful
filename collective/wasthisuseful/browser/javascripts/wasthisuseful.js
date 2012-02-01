aacd712f43d6f011b555a0fc7c1a59e1ea1de671
Wietze Helmantel <helmantel@gw20e.com>
2012-02-01 12:18:15 +0100

    removed unused function

--- collective/wasthisuseful/browser/javascripts/wasthisuseful.js
+++ collective/wasthisuseful/browser/javascripts/wasthisuseful.js
@@ -36,19 +36,9 @@ var wtu = {
             }
         })    
     },
-    
-    /* When "No" is clicked (information is not useful), show the comment field 
-     * and submit button. */
-    expandForm: function() {
-        jq('#useful input:radio[value="0"]').click(function() {
-            jq(this).parent().find(".comments").fadeIn();
-        }); 
-    } // no comma after last item
-
 }
 
 jq(document).ready(function() {
-    wtu.expandForm();
     wtu.thumbs();
 });
 
