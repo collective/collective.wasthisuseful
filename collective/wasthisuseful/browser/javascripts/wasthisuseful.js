06cc8e973420036cfe2aef0b7ed5ed59bf305900
Davi Lima de Medeiros <davilima6@gmail.com>
2013-08-23 14:45:13 -0300

    jq is not used since Plone 4.0b2

--- collective/wasthisuseful/browser/javascripts/wasthisuseful.js
+++ collective/wasthisuseful/browser/javascripts/wasthisuseful.js
@@ -2,43 +2,43 @@ var wtu = {
 
     thumbs: function() {
         
-        jq("#div-submit").click(function() {
-           jq("#form-comment").attr('value', jq("#div-comment").attr('value'));  
-           jq('#usefulness-form').submit();
+        $("#div-submit").click(function() {
+           $("#form-comment").attr('value', $("#div-comment").attr('value'));  
+           $('#usefulness-form').submit();
         });
         
-        jq('.javascript-only').css('display', 'inline');
-        jq('#usefulness-form').hide();
+        $('.javascript-only').css('display', 'inline');
+        $('#usefulness-form').hide();
 
-        jq('.relevance a').click(function(e) {
+        $('.relevance a').click(function(e) {
             
             e.preventDefault();
             
             // reset, read thumb, and check according field
-            jq('input[name=useful]').attr('checked', false);
+            $('input[name=useful]').attr('checked', false);
 
-            var thumb = jq(this).attr('class');
-            var up = jq('.up');
-            var down = jq('.down');
-            var whynot = jq("#why-not-useful");
+            var thumb = $(this).attr('class');
+            var up = $('.up');
+            var down = $('.down');
+            var whynot = $("#why-not-useful");
             
             if( thumb == 'up' ) {
                 up.css('background-position', '5px -30px');
                 down.css('background-position', '-50px 0');
                 whynot.hide();
-                jq('input#yes').attr('checked', true);
-                jq('#usefulness-form').submit();
+                $('input#yes').attr('checked', true);
+                $('#usefulness-form').submit();
             } else {
                 down.css('background-position', '-50px -30px');
                 up.css('background-position', '5px 0');
-                jq('input#no').attr('checked', true);
+                $('input#no').attr('checked', true);
                 whynot.fadeIn();
             }
         }); 
     }
 };
 
-jq(document).ready(function() {
+$(document).ready(function() {
     wtu.thumbs();
 });
 
